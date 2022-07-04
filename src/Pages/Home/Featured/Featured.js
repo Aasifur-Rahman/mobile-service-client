38a4199d02ee4e272f57d2d0511051af3e034bf2
AsifurRahman66544 <93776889+AsifurRahman66544@users.noreply.github.com>
2022-07-04 20:21:22 +0600

    featured section added

---
+++ src/Pages/Home/Featured/Featured.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+export default class AutoPlay extends Component {
+    render() {
+        const settings = {
+            dots: true,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            speed: 4000,
+            autoplaySpeed: 1000,
+            cssEase: "linear"
+        };
+        return (
+            <div>
+                <h2 className='text-center mt-5 mb-4' >Featured</h2>
+                <Slider {...settings}>
+                    <div>
+                        <h3>1</h3>
+                    </div>
+                    <div>
+                        <h3>2</h3>
+                    </div>
+                    <div>
+                        <h3>3</h3>
+                    </div>
+                    <div>
+                        <h3>4</h3>
+                    </div>
+                    <div>
+                        <h3>5</h3>
+                    </div>
+                    <div>
+                        <h3>6</h3>
+                    </div>
+                </Slider>
+            </div>
+        );
+    }
+}
