6abe2dd715615a9a760ee193e4e252137db8edab
AsifurRahman66544 <93776889+AsifurRahman66544@users.noreply.github.com>
2022-07-14 16:29:43 +0600

    nav bar complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Home from './Pages/Home/Home/Home';
 import Header from './Pages/Shared/Header/Header';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header></Header>
       <Home></Home>
-    </div>
+    </Router>
   );
 }
 
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './Header.css';
+import image from './header logo/mobile service logo.png'
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import image from './header logo/mobile service logo2.png'
+
+
 const Header = () => {
     return (
         <div>
-            <Navbar light bgColor='light' expand="lg">
+            <Navbar expand="lg">
                 <Container>
-                    <Navbar.Brand className='text-white px-3' href="#home"><img height={30} className='logo-img' src={image} alt="" /></Navbar.Brand>
+                    <Navbar.Brand className='text-white px-3' href="#home"><img width={40} className='logo-img' src={image} alt="" /></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mx-auto text-center fs-6 ">
-                            <Nav.Link className='different pe-5  text-black' href="#home">Home</Nav.Link>
-                            <Nav.Link className='different pe-5   text-black' href="#link">Services</Nav.Link>
-                            <Nav.Link className='different pe-5   text-black' href="#link">About</Nav.Link>
-                            <Nav.Link className='different  text-black' href='#link'>Contact <span>Us</span></Nav.Link>
+                        <Nav className="mx-auto ps-3 fs-6 ">
+                            <Nav.Link className='different pe-5 custom-text' href="#home">Home</Nav.Link>
+                            <Nav.Link className='different pe-5  custom-text' href="#link">Services</Nav.Link>
+                            <Nav.Link className='different pe-5  custom-text' href="#link">About</Nav.Link>
+                            <Nav.Link className='different pe-5  custom-text' href='#link'>Contact <span>Us</span></Nav.Link>
+
                         </Nav>
-                        <Nav.Link className='different  text-black' href='#link'>Login</Nav.Link>
+                        <Nav.Link className='different pe-5  custom-text' href='#link'>Login</Nav.Link>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
